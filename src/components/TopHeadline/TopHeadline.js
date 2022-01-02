5e9a388ae147d564603d47cc1dfcd7cb5778e12c
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-01-02 12:30:25 +0600

    using axios get data

--- src/components/TopHeadline/TopHeadline.js
+++ src/components/TopHeadline/TopHeadline.js
@@ -1,19 +1,33 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import News from '../News/News';
 
 const TopHeadline = () => {
-    const url= 'https://newsapi.org/v2/everything?q=tesla&from=2021-12-01&sortBy=publishedAt&apiKey=c371b5c7269e4db5b3773909f0635dce'
-    const [articles, setArticles]= useState([]);
-    useEffect(()=>{
-        fetch(url)
-        .then( res=> res.json())
-        .then(data=>setArticles(data.articles))
-    },[])
+
+    const todayDate = new Date().toISOString().slice(0, 10);
+    const url = 'https://newsapi.org/v2/everything?q=tesla&from=' + todayDate + '&sortBy=publishedAt&apiKey=c371b5c7269e4db5b3773909f0635dce'
+    const url3 = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c371b5c7269e4db5b3773909f0635dce'
+    const url4 = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=c371b5c7269e4db5b3773909f0635dce'
+    const [articles, setArticles] = useState([]);
+    const [articles2, setArticles2] = useState([]);
+    useEffect(() => {
+        fetch(url4)
+            .then(res => res.json())
+            .then(data => setArticles(data.articles))
+    }, [])
+
+    useEffect(() => {
+        const url2 = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c371b5c7269e4db5b3773909f0635dce'
+        axios.get(url2)
+            .then(data => setArticles2(data.data.articles) )
+
+    }, [])
     return (
         <div>
-             <h2 className='text-center text-success p-2' >Top Heading: {articles.length}</h2>
+            <h2 className='text-center text-success p-2' >Top Heading: {articles.length}</h2>
             {
-                articles.map(article=><News article ={article}></News>)
+               // articles.map(article => <News key={article.title}  article={article}></News>)
+                articles2.map(article => <News key={article.title} article={article}></News>)
             }
         </div>
     );
